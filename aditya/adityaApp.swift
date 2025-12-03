6dae103144928225917f27d4c4f7ee4ae69b5ec7
DurganandSah <sahdurganand38@gmail.com>
2025-12-03 13:22:36 +0530

    Initial Commit

---
+++ aditya/adityaApp.swift
@@ -0,0 +1,17 @@
+//
+//  adityaApp.swift
+//  aditya
+//
+//  Created by student on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct adityaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
